8690189c237a03dd4ff5110162faaf9239da9569
mariduartee <marih988@hotmail.com>
2025-03-16 22:21:13 -0300

    AGENDA 4

--- MauiAppMinhasCompras/Helpers/SQLiteDataBaseHelper.cs
+++ MauiAppMinhasCompras/Helpers/SQLiteDataBaseHelper.cs
@@ -55,7 +55,7 @@ bloquear a thread principal*/
 permitindo filtrar registros de acordo com uma string fornecida*/
         public Task<List<Produto>> Search(string q) 
         {
-            string sql = "SELECT * Produto WHERE descricao LIKE '%" + q + "%'";
+            string sql = "SELECT * FROM Produto WHERE descricao LIKE '%" + q + "%'";
 
             return _conn.QueryAsync<Produto>(sql);
         }
